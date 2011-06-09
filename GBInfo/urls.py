b3843248c4f444d71611bff39e6cf97be03cd409
Rosa Durante <rosunix@gmail.com>
2011-06-09 13:48:35 +0200

    Add the isbndb app into urls

--- GBInfo/urls.py
+++ GBInfo/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'GBInfo.views.home', name='home'),
+    url(r'^$', include('GBInfo.isbndb.urls')),
     # url(r'^GBInfo/', include('GBInfo.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
